b20f09ec701b628855b610d5769a3c852d9a85fd
Max Sveshnikov <msveshnikov@thumbtack.net>
2017-03-21 23:38:44 +0600

    Remove crap

---
+++ TextParserMvc/Models/Sentence.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TextParserMvc.Models
+{
+    class Sentence
+    {
+        private List<string> words;
+
+        public List<string> Words
+        {
+            get { return words.OrderBy(x => x.ToLower()).ToList(); }
+            set { this.words = value; }
+        }
+    }
+}
--- TextParserMvc/Startup.cs
+++ TextParserMvc/Startup.cs
@@ -8,7 +8,6 @@ namespace TextParserMvc
     {
         public void Configuration(IAppBuilder app)
         {
-            ConfigureAuth(app);
         }
     }
 }
